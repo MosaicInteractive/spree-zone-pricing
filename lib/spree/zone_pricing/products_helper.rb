d61ba27ddb5ed76f759a7e39d83d71d3cda57794
Mosaic Interactive <valentino@mosaicwebsite.com>
2011-02-18 08:51:56 -0500

    Added wholesale compatability

--- lib/spree/zone_pricing/products_helper.rb
+++ lib/spree/zone_pricing/products_helper.rb
@@ -11,16 +11,22 @@ module Spree::ZonePricing::ProductsHelper
     options.assert_valid_keys(:format_as_currency, :show_vat_text)
     options.reverse_merge! :format_as_currency => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
 
-    # Check if this object is a product, if so then access the master variant
-    # record for the product to get the zone pricing
-    object = product_or_variant.is_a?(Product) ? product_or_variant.master : product_or_variant
-
-    # Get the zone price for this variant/product if one is defined
-    # otherwise use the normal price
-    if object.respond_to?(:zone_price)
-      amount = object.zone_price(get_user_country_id)
+    if defined?(product_or_variant.wholesale_price) and
+        (!current_user.nil? && current_user.has_role?("wholesale") && !product_or_variant.wholesale_price.blank?)
+      amount = product_or_variant.wholesale_price
     else
-      amount = object.price
+
+      # Check if this object is a product, if so then access the master variant
+      # record for the product to get the zone pricing
+      object = product_or_variant.is_a?(Product) ? product_or_variant.master : product_or_variant
+
+      # Get the zone price for this variant/product if one is defined
+      # otherwise use the normal price
+      if object.respond_to?(:zone_price)
+        amount = object.zone_price(get_user_country_id)
+      else
+        amount = object.price
+      end
     end
 
     amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
